51602b3f4358c33b386d4b5ac4fa723c269caaec
Gabriel-Delazeri <gbldelazeri@gmail.com>
2022-11-02 08:17:13 -0300

    feat: fix flash-messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,7 @@
         <main class="py-4">
             <div class="container">
                 <div class="row">
+                    @include('flash-message')
                     @yield('content')
                 </div>
             </div>
